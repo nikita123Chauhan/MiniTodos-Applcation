6098537a67b02a90efd5638c656d6253abc05299
Nikita Chauhan <12nikitachauhan2004@gmail.com>
2024-12-29 22:43:45 +0530

    mini todos application

---
+++ frontend/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("footer").innerHTML = `
+  <footer>
+    <p>&copy; 2024 Mini Todo App</p>
+  </footer>
+`;
---
+++ frontend/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML = `
+  <nav>
+    <a href="index.html">Login</a> |
+    <a href="signup.html">Signup</a> |
+    <a href="todos.html">Todos</a>
+  </nav>
+`;
